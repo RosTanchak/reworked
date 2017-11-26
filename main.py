8b3b336d623dcc9e647b1f7a9412ca782f633210
Rostyslav Tanchak <rostyslav.tanchak@keenethics.com>
2017-11-26 21:21:39 +0200

    New way for calc

--- main.py
+++ main.py
@@ -4,8 +4,11 @@
 from PyQt4 import QtGui
 import sys
 import math
+import os
+
 
 import ui_design #import form from '/reworked/ui_design.py'
+from  all_calculations import All_calculations #import all math functions
 
 
 
@@ -14,17 +17,15 @@ class PraktykaApp(QtGui.QMainWindow, ui_design.Ui_MainWindow):
         super(PraktykaApp, self).__init__(parent)
         self.setupUi(self)
         self.pushButton_5.clicked.connect(self.open_file)
+        self.pushButton_6.clicked.connect(self.save_file)
+        self.pushButton_7.clicked.connect(self.build)
 
     def open_file(self):
         filename = QtGui.QFileDialog.getOpenFileName()
         if filename:
             filename = unicode(filename, "utf-8")
             text = open(filename).read()
-            print("filename", text)
-            # self.originalText = text.decode("utf-8")
-            # global L
-            # L = len(self.originalText)
-            # self.splitText(self.originalText)
+            self.all_calculations = All_calculations(text)
 
     def save_file(self):
         name = QtGui.QFileDialog.getSaveFileName()
@@ -33,16 +34,32 @@ class PraktykaApp(QtGui.QMainWindow, ui_design.Ui_MainWindow):
         self.file2 = open(name + ":dti.txt", 'w' )
         self.file3 = open(name + ":Ti.txt", 'w' )
         self.file4 = open(name + ":NewSymbols.txt", 'w' )
-        self.setTab1 = True
-        self.setTab2 = True
-        self.setTab3 = True
-        self.setTab4 = True
 
-        #SAVE IMplamentation
-        # self.table1()
-        # self.fillTableTab2()
-        # self.fillTableTab3()
-        # self.buildTable4()
+    def build(self):
+        # self.completed = 0
+        # while self.completed < 100:
+        #     self.completed += 0.00001
+        #     self.progressBar.setValue(self.completed)
+        #     self.tabWidget.setTabEnabled(1, False)
+
+        prepearedText = self.all_calculations.splitText(4)
+        new_symbols_Text = self.all_calculations.calcNewSymbols(prepearedText)
+        print("hui", new_symbols_Text)
+        self.tabWidget.setTabEnabled(1, True)
+
+
+    def fill_table(self, tableNumber):
+        if tableNumber == 1:
+            print("1")
+        elif tableNumber == 2:
+            pass
+        elif tableNumber == 3:
+            pass
+        elif tableNumber == 4:
+            pass
+        else:
+            return
+
 
 
 
